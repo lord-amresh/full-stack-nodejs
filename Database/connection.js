667801461048ea878a776b223e7ffe8e33397878
lord-amresh <amresh2006@gmail.com>
2025-05-10 08:58:51 +0545

    may 10 9am

--- Database/connection.js
+++ Database/connection.js
@@ -4,28 +4,29 @@ const {Sequilize,DataTypes} = require("sequelize")
 
 //const bookModel = require("./Models/book.model")
 
- const sequelize = new Sequelize("postgresql://postgres.bpurtwvuskghtnohkvwf:rabindra2229@aws-0-ap-south-1.pooler.supabase.com:6543/postgres")
+ const sequelize = new Sequelize
+ ("postgresql://postgres.bpurtwvuskghtnohkvwf:rabindra2229@aws-0-ap-south-1.pooler.supabase.com:6543/postgres")
 
  sequelize.authenticate()
  .then(()=>{
     console.log("Authenticated vayo, connected vayo!!")
- })jjj
+ })
  .catch((err)=>{
     console.log("Error aayo" + err)
  })
 
 
  const db = {}
- db.Sequelize = sequelize
- db.sequilize = sequilize
+ db.Sequelize = Sequelize
+ db.sequelize = sequelize
 
- db.books = require("./Models/book.model")(sequilize,DataTypes)
+ db.books = require("./Models/book.model")(sequelize,DataTypes)
 
 
  //migrate code ho yo chai 
  // need to pdate and complete this
 
- sequilize.sync({force : false}).then(()=>{
+ sequelize.sync({force : false}).then(()=>{
     console.log("Migrate vayo hai ta")
 })
 
