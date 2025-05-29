ec590a8e6cd37f673f8cbddba8cc67cbdbbc2762
lord-amresh <amresh2006@gmail.com>
2025-05-29 08:15:17 +0545

    api testing and integration

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express") // express require gareko
+const { books } = require("./database/db.connection")
 const app = express()  //express lai trigger gareko
 
 require("./database/db.connection")
 
 //let app = require("express")()
-app.get("/books",function(req,res){
+app.get("/books",async function(req,res){
     //logic to fetch books from database
-    users.findall()
+
+    const datas = await books.findAll() // select * from books, for mongodb- books.find(), alaways returns array
     res.json({
-        message :"books fetched successfully"
+        message :"books fetched successfully",
+        datas
     })
 })
 
@@ -31,4 +34,8 @@ app.patch("/books/:id",function(req,res){
     res.json({
         message : "Book deleted successfully"
     })
+})
+
+app.listen(3000, function (){
+    console.log("project/server/backend has started at port 3000")
 })
