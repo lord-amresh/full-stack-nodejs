603065bdb8c1f295857026ec345c858e19edff53
lord-amresh <amresh2006@gmail.com>
2025-05-30 18:24:53 +0545

    single page frontend api

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express") // express require gareko
 const bookRoute = require("./routes/bookRoute")
-
+const cors = require("cors")
 
 const app = express()  //express lai trigger gareko
 require("./database/db.connection")
+
+app.use(cors({
+    origin : "*"
+}))
 app.use(express.json())
 //let app = require("express")()
 
